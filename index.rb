05fc234da503f4b7409d2e1faf0c9e8dc004e6fd
Jose <jose.muhlanga@student.moringaschool.com>
2023-03-20 17:52:14 +0300

    Final Commit

--- index.rb
+++ index.rb
@@ -1,3 +1,11 @@
-def camel_case(string)
-    string.split.map(&:capitalize).join
+class String
+    def camelcase
+      split.map(&:capitalize).join
+    end
 end
+
+string1 = 'hello case';
+string2 = 'camel case word'
+
+puts string1.camelcase;
+puts string2.camelcase;
